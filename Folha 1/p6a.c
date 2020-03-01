90f6540deac023bb82eda6d0ef8a5fee12a99421
Nunation <oliveiranunopt@gmail.com>
2020-03-01 17:10:38 +0000

    finished p6

--- Folha 1/p6a.c
+++ Folha 1/p6a.c
@@ -8,16 +8,21 @@
 int main(int argc,char *argv[]) {   
 	FILE *src, *dst;   
 	char buf[BUF_LENGTH];
+
 	char *infile = argv[1];
 	char *outfile = argv[2];
 
-	if ( ( src = fopen( "infile.txt", "r" ) ) == NULL )    {
+	if(argc!=3){
+		printf("Error!! Usage: nome_do_executável file1 file2\n");
+	}
+
+	if ( ( src = fopen( infile, "r" ) ) == NULL )    {
 		//perror("Error");
 		//printf("Error: %s\n", strerror(errno));
 		printf("Error: %d\n", errno);
 	    exit(1);     
 	 }   
-	 if ( ( dst = fopen( "outfile.txt", "w" ) ) == NULL )   { 
+	 if ( ( dst = fopen( outfile, "w" ) ) == NULL )   { 
 	    exit(2);  
 	}   
 	while( ( fgets( buf, BUF_LENGTH+1, src ) ) != NULL )   { 
