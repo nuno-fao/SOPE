730bc096c2485e06ea597670b88be2322867236b
Nunation <oliveiranunopt@gmail.com>
2020-03-09 17:20:07 +0000

    Created Folha3, made p1

---
+++ Folha3/p1.c
@@ -0,0 +1,22 @@
+//PROGRAMA p1.c
+
+#include <stdio.h>
+#include <unistd.h>
+
+int global = 1;
+
+int main(void){
+	int local=2;
+	if(fork()>0){
+		printf("PID = %d; PPID = %d\n",getpid(),getppid());
+		global++;
+		local--;
+	}
+	else{
+		printf("PID = %d; PPID = %d\n",getpid(),getppid());
+		global--;
+		local++;
+	}
+	printf("PID = %d - global = %d ; local = %d\n",getpid(),global,local);
+	return 0;
+}
